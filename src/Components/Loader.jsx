bc91847b0bff244986b59207e9180ab813fa8b78
Mahdi Jahed <mahdijahed56@gmail.com>
2025-12-11 12:40:18 +0330

    feat: Construction of a spiner loader

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="w-full py-20 flex justify-center items-center">
+      <div className="animate-spin w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full"></div>
+    </div>
+  );
+}
