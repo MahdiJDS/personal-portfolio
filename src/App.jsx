accffb1ae90d83f0b8e923bdb7b7321418e57fea
Mahdi Jahed <mahdijahed56@gmail.com>
2025-12-11 12:41:50 +0330

    feat: Using lazy loading and Suspense to optimize the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import Loader from './Components/Loader'
 import Navbar from './Components/navbar';
-import Hero from './Components/Hero';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
-import Skills from './Components/Skills';
-import Footer from './Components/Footer';
-import { ThemeProvider } from "./Context/ThemeContext";
 import UpButton from './Components/UpButton';
+import { ThemeProvider } from "./Context/ThemeContext";
+
+const Hero = lazy(()=>import('./Components/Hero'))
+const About = lazy(() => import('./Components/About'));
+const Projects = lazy(() => import('./Components/Projects'));
+const Skills = lazy(() => import('./Components/Skills'));
+const Contact = lazy(() => import('./Components/Contact'));
+const Footer = lazy(() => import('./Components/Footer'));
 
 
 
 function App() {
   return (
-      <ThemeProvider>
-        <Navbar />
+    <ThemeProvider>
+      <Navbar />
+
+       <Suspense fallback={<Loader />}>
         <Hero />
+      </Suspense>
+
+      <Suspense fallback={<Loader />}>
         <About />
+      </Suspense>
+
+      <Suspense fallback={<Loader />}>
         <Projects />
+      </Suspense>
+
+      <Suspense fallback={<Loader />}>
         <Skills />
+      </Suspense>
+
+      <Suspense fallback={<Loader />}>
         <Contact />
+      </Suspense>
+
+      <Suspense fallback={<Loader />}>
         <Footer />
-        <UpButton />
-      </ThemeProvider>
+      </Suspense>
+
+      <UpButton />
+    </ThemeProvider >
   );
 }
 
